7618d98bb18fa5b5c47ce778a669b24447fa1a70
Narmadhu <oknarmadhu10496@gmail.com>
2020-03-03 11:06:11 +0530

    Updates

---
+++ precache-manifest.1d22686ac17992e395881cb5e92614bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a13726bed6a523394632abc6ff854d5",
+    "url": "/FoodRecipeSearch-API/index.html"
+  },
+  {
+    "revision": "65d0d7d149ae68a88b58",
+    "url": "/FoodRecipeSearch-API/static/css/main.6d70554a.chunk.css"
+  },
+  {
+    "revision": "46c0ce06eb8e5d6ab11a",
+    "url": "/FoodRecipeSearch-API/static/js/2.a2271bf2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/FoodRecipeSearch-API/static/js/2.a2271bf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d0d7d149ae68a88b58",
+    "url": "/FoodRecipeSearch-API/static/js/main.4a7295b9.chunk.js"
+  },
+  {
+    "revision": "b16969e203b199ff571d",
+    "url": "/FoodRecipeSearch-API/static/js/runtime-main.d12d1a58.js"
+  }
+]);
